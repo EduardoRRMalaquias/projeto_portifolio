90776b0c978fdea243e98b98d9c7c8d37704a5f5
Eduardo Rodrigues Rangel Malaquias <malaquias.eduardo18@gmail.com>
2024-06-30 18:51:08 -0300

    diferenciais

--- js/scripts.js
+++ js/scripts.js
@@ -91,3 +91,21 @@ function eventoCopia(copyArea) {
 }
 copyAreas.forEach(eventoCopia);
 
+// diferenciais
+
+const diferenciais = document.querySelectorAll(".diferencial-item button");
+
+function ativarDesativarDiferencial(event) {
+  const diferencial = event.currentTarget;
+  const controls = diferencial.getAttribute("aria-controls");
+  const descricao = document.getElementById(controls);
+
+  descricao.classList.toggle("ativo");
+  const ativo = descricao.classList.contains("ativo");
+  diferencial.setAttribute("aria-expanded", ativo);
+}
+
+function clickDiferenciais(diferencial) {
+  diferencial.addEventListener("click", ativarDesativarDiferencial);
+}
+diferenciais.forEach(clickDiferenciais);
