72e14ed0068f9ede6f2abd60733700b814c4a624
Eduardo Rodrigues Rangel Malaquias <malaquias.eduardo18@gmail.com>
2024-09-23 10:39:16 -0300

    modeo claro escuro provisorio

--- js/modules/modo-ligth-dark.js
+++ js/modules/modo-ligth-dark.js
@@ -3,23 +3,46 @@ export default class DarkLigthMode {
     this.btnDarkLightMode = document.querySelector(btnDarkLightMode);
     this.checkDarkLightMode = document.querySelector(checkDarkLightMode);
     this.temaUsuario = window.matchMedia("(prefers-color-scheme: dark)");
+    this.temaSalvo = localStorage.getItem("temaUsuario");
+
+    this.mudarTemaCheck = this.mudarTemaCheck.bind(this);
+    this.detectarPreferenciaSistema =
+      this.detectarPreferenciaSistema.bind(this);
   }
 
   ativarDesativarCheck(ativacao) {
     this.checkDarkLightMode.checked = ativacao;
   }
 
-  detectarTema() {
-    this.temaUsuario.addEventListener("change", () => {
-      this.ativarDesativarCheck(this.temaUsuario.matches);
-    });
+  removerAddClasse(ativacaoLigth, ativacaoDark) {
+    const html = document.querySelector("html");
+    html.classList[ativacaoLigth]("ligth");
+    html.classList[ativacaoDark]("dark");
+  }
+
+  detectarPreferenciaSistema() {
+    this.removerAddClasse("remove", "remove");
+    this.ativarDesativarCheck(this.temaUsuario.matches);
+  }
+
+  mudarTemaCheck() {
+    if (this.checkDarkLightMode.checked) {
+      this.removerAddClasse("remove", "add");
+    } else {
+      this.removerAddClasse("add", "remove");
+    }
+  }
+
+  addEventosTema() {
+    this.temaUsuario.addEventListener(
+      "change",
+      this.detectarPreferenciaSistema
+    );
+    this.checkDarkLightMode.addEventListener("change", this.mudarTemaCheck);
   }
 
   init() {
-    this.ativarDesativarCheck(this.temaUsuario.matches);
-    console.log(this.btnDarkLightMode);
-    console.log(this.checkDarkLightMode);
-    console.log((this.temaUsuario.onchange = true));
-    this.detectarTema();
+    this.detectarPreferenciaSistema();
+    this.addEventosTema();
   }
 }
